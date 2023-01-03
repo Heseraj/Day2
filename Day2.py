3f110601d5baa6146aa36626023ab4d1ed7b720d
HeSer <B@nd@r1359>
2023-01-03 12:01:09 -0500

    update v2

--- Day2.py
+++ Day2.py
@@ -17,13 +17,31 @@ print("Hello Mosa!")
 
 # ## body max
 
-weight = input("what is your weight KG? \n ")
-height = input("What is your height in meter? \n")
-height = float(height)
-print(type(height))
+# weight = input("what is your weight KG? \n ")
+# height = input("What is your height in meter? \n")
+# height = float(height)
+# print(type(height))
+#
+# your_BMI = int(weight)/(height**2)
+#
+# your_BMI = round(your_BMI, 2)
+#
+# print("Your BMI is: ", your_BMI)
 
-your_BMI = int(weight)/(height**2)
+# Good, Keep practicing, you will become a natural in coding
+#
+# score = 0
+# height = 1.8
+# isWining = True
+#
+# print(f"your score is {score}")
 
-print("Your BMI is: ", your_BMI)
+age = int(input("How old are you? \n"))
+# print(type(age))
+max_age = 90
 
-# Good, Keep practicing, you will become a natural in coding 
+years_left = max_age - age
+month_left = years_left * 12
+days_left = years_left * 365
+
+print(f"you are {age} now. It means you got {years_left} left. This is equal to {month_left} months and {days_left} days. Make the most of out your remaining days and months.")
