845606cfbcf6f2e40713b5359cd81b13cd7fff6f
HeSer <B@nd@r1359>
2023-01-03 11:04:56 -0500

    Update v1

--- Day2.py
+++ Day2.py
@@ -1,3 +1,29 @@
 
 print("Hello Mosa!")
-# This is the start of second day 
+# This is the start of second day
+
+# number = input("type a two digit number \n")
+# print(type(number))
+# num_int = int(number)
+# num_str = str(num_int)
+# num_str = str(number)
+
+
+# print(type(number))
+
+
+# print(int(number[0]) + int(number[1]))
+
+
+# ## body max
+
+weight = input("what is your weight KG? \n ")
+height = input("What is your height in meter? \n")
+height = float(height)
+print(type(height))
+
+your_BMI = int(weight)/(height**2)
+
+print("Your BMI is: ", your_BMI)
+
+# Good, Keep practicing, you will become a natural in coding 
